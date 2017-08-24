84b27db33e1a82ad3cfb6f83a90930067cd2b32d
Maks Lesyk <maksym@lesyk.org>
2017-08-24 18:02:02 +0300

    web application configs/stuff

---
+++ src/main/java/org/mlesyk/Loggers.java
@@ -0,0 +1,14 @@
+package org.mlesyk;
+
+/**
+ * Created by Maks on 20.08.2017.
+ *
+ * Place for all loggers in project
+ */
+public class Loggers {
+
+    public static final String SNMP = "netmon.server.snmp";
+
+    public static final String SERVER = "netmon.server";
+
+}
